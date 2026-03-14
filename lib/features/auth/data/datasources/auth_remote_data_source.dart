e99e9ef53db19e0156c9de118411dc15852a0746
Md Asif Khan <asif.khan08@northsouth.edu>
2026-03-14 13:25:02 +0600

    modified

--- lib/features/auth/data/datasources/auth_remote_data_source.dart
+++ lib/features/auth/data/datasources/auth_remote_data_source.dart
@@ -5,18 +5,12 @@ import '../models/login_model.dart';
 import '../models/register_model.dart';
 import '../models/user_model.dart';
 
-abstract class AuthRemoteDataSource {
-  Future<AuthTokenModel> login(LoginModel data);
-  Future<AuthTokenModel> register(RegisterModel data);
-  Future<UserModel> fetchMe(String token);
-}
-
-class AuthRemoteDataSourceImpl implements AuthRemoteDataSource {
-  AuthRemoteDataSourceImpl({required this.client});
+class AuthRemoteDataSource{
+  AuthRemoteDataSource({required this.client});
 
   final http.Client client;
 
-  @override
+
   Future<AuthTokenModel> login(LoginModel data) async {
     final url = Uri.parse('http://192.168.0.106:8000/auth/token/login/');
     final response = await client.post(
@@ -32,7 +26,7 @@ class AuthRemoteDataSourceImpl implements AuthRemoteDataSource {
     throw Exception('Login failed');
   }
 
-  @override
+
   Future<AuthTokenModel> register(RegisterModel data) async {
     final url = Uri.parse('http://192.168.0.106:8000/auth/users/');
     final response = await client.post(
@@ -48,7 +42,7 @@ class AuthRemoteDataSourceImpl implements AuthRemoteDataSource {
     throw Exception('Registration failed');
   }
 
-  @override
+
   Future<UserModel> fetchMe(String token) async {
     final url = Uri.parse('http://192.168.0.106:8000/auth/users/me/');
     final response = await client.get(
